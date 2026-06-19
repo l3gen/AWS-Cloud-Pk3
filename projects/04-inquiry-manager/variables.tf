9c80b1f44c261ff87d6cdc17da062aa8d53f6aff
Deandre Williams <deandrewilliams656@yahoo.com>
2026-06-19 17:04:56 -0400

    fix: alert_email variable multi-line block (no semicolons)

--- projects/04-inquiry-manager/variables.tf
+++ projects/04-inquiry-manager/variables.tf
@@ -1,3 +1,6 @@
 variable "aws_region"   { default = "us-east-1" }
-variable "alert_email" { description = "Email for urgent inquiry alerts"; default = "dw602481@gmail.com" }
+variable "alert_email" {
+  description = "Email for urgent inquiry alerts"
+  default     = "dw602481@gmail.com"
+}
 variable "project_name" { default = "inquiry-manager" }
