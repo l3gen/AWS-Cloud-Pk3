ee6645422df6e37d191161dc584dc7993b43d7f1
Deandre Williams <deandrewilliams656@yahoo.com>
2026-06-19 16:59:40 -0400

    fix: add default for alert_email so CI does not prompt

--- projects/04-inquiry-manager/variables.tf
+++ projects/04-inquiry-manager/variables.tf
@@ -1,3 +1,3 @@
 variable "aws_region"   { default = "us-east-1" }
-variable "alert_email"  { description = "Email for urgent inquiry alerts" }
+variable "alert_email" { description = "Email for urgent inquiry alerts"; default = "dw602481@gmail.com" }
 variable "project_name" { default = "inquiry-manager" }
